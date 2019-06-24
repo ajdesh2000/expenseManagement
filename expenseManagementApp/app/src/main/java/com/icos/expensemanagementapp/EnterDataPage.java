277bb0f3af788a5a8683704a94b32c280906d407
ajdesh <ajdesh2000@gmail.com>
2019-06-24 14:10:10 +0530

    first commit

---
+++ expenseManagementApp/app/src/main/java/com/icos/expensemanagementapp/EnterDataPage.java
@@ -0,0 +1,14 @@
+package com.icos.expensemanagementapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnterDataPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_data_page);
+    }
+}
---
+++ expenseManagementApp/app/src/main/java/com/icos/expensemanagementapp/ShowDataPage.java
@@ -0,0 +1,14 @@
+package com.icos.expensemanagementapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowDataPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_data_page);
+    }
+}
